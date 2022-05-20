4b4dcea1e3c86a56de76df7becb2f9cb469cbffc
박성원 <ppsy1547@gmail.com>
2022-05-20 14:30:34 +0000

    날씨정보, 옷 추천 카드 완성

--- kakaoBot.py
+++ kakaoBot.py
@@ -3,6 +3,9 @@ from flask import Flask, request, jsonify
 
 kakaoBot = Flask(__name__)
 
+global outputFits #복장 목록
+outputFits = ""
+
 @kakaoBot.route("/")
 def hello():
     return "Hello goorm!"
@@ -10,18 +13,19 @@ def hello():
 @kakaoBot.route("/weather",methods=['POST'])
 def weatherPrint():
     req = request.get_json()
-    
+        
     location = req["action"]["detailParams"]["sys_location"]["value"]	# json파일 읽기
     
     temp = Weather(location)
     
-    answer = temp.getWeather()
+    answer = temp.getWeather() 
     
-    outputFits = "==== 오늘 기온에 맞는 옷 추천 ====\n"
+    global outputFits
+    outputFits = "[오늘 기온에 맞는 옷 추천]\n"
     for fits in temp.getFits():
         outputFits += fits.replace('"','').strip() + "\n"
     
-    answerFits = outputFits
+    #https://img.freepik.com/free-vector/clothes-cartoon_119631-167.jpg 카드 이미지
     
     # 답변 텍스트 설정
     res = {
@@ -34,16 +38,80 @@ def weatherPrint():
                     }
                 },
                 {
-                    "simpleText": {
-                        "text": answerFits
-                    }
-                }
+        "basicCard": {
+          "title": "오늘의 추천 복장은?",
+          "thumbnail": {
+            "imageUrl": "https://img.freepik.com/free-vector/clothes-cartoon_119631-167.jpg"
+          },
+          "buttons": [
+            {
+              "action": "message",
+              "label": "옷 추천 목록 보기",
+              "messageText": "옷 추천"
+            }
+          ]
+        }
+      }
             ]
         }
     }
 
     # 답변 전송
     return jsonify(res)
+
+@kakaoBot.route('/message', methods=['POST'])
+def Message():
+    content = request.get_json()
+    content = content['userRequest']['utterance'] #사용자 발화값 받아오기
+    content=content.replace("\n","")
+    
+    global outputFits
+    
+    if content.find("옷") != -1 or content.find("외출복") != -1 or content.find("복장") != -1:
+        content = "옷"
+    
+    if outputFits == "":
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "지역이름을 먼저 검색해주세요"
+                        }
+                    }
+                ]
+            }
+        }
+        return jsonify(dataSend)
+    
+    if content == u"옷":
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : outputFits
+                        }
+                    }
+                ]
+            }
+        }
+    else:
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "error입니다."
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
 
 if __name__ == "__main__":
     kakaoBot.run(host="0.0.0.0", port=5000, threaded=True)
---
+++ store.py
@@ -0,0 +1,29 @@
+import requests
+
+api_key = 'b57c89bcdaa776e98f2de85501de1a4b'
+totals = list()
+
+def addr_to_lat_lon(region):
+    page_num = 1
+    placeName = list()
+    placeAddr = list()
+    placeUrl = list()
+    url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
+    params = {'query': region,'page': page_num,'category_group_code' : 'CS2'}
+    headers = {"Authorization": "KakaoAK " + api_key}
+    places = requests.get(url, params=params, headers=headers).json()['documents']
+    
+    for i in range(0, len(places)):
+        placeName.append(places[i]['place_name'])
+        placeAddr.append(places[i]['address_name'])
+        placeUrl.append(places[i]['place_url'])
+
+    total = list(zip(placeName, placeAddr, placeUrl))
+
+    return total
+
+store = addr_to_lat_lon('하양역 주변 편의점')
+
+print(store)
+
+
