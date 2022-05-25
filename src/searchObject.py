b86c77c52be843716ded92c5ddc350ebb4746664
myeongsu0923 <102271691+myeongsu0923@users.noreply.github.com>
2022-05-25 20:54:37 +0900

    Update searchObject.py

--- src/searchObject.py
+++ src/searchObject.py
@@ -1,28 +1,29 @@
 import requests
 import random
 
-api_key = 'b57c89bcdaa776e98f2de85501de1a4b'
-totals = list()
+# 편의점 추천 사용중단. 
+# api_key = 'api_key입력'
+# totals = list()
 
-def storeSearch(region):
-    page_num = 1
-    region += '주변 편의점'
-    placeName = list()
-    placeAddr = list()
-    placeUrl = list()
-    url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
-    params = {'query': region,'page': page_num,'category_group_code' : 'CS2'}
-    headers = {"Authorization": "KakaoAK " + api_key}
-    places = requests.get(url, params=params, headers=headers).json()['documents']
+# def storeSearch(region):
+#     page_num = 1
+#     region += '주변 편의점'
+#     placeName = list()
+#     placeAddr = list()
+#     placeUrl = list()
+#     url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
+#     params = {'query': region,'page': page_num,'category_group_code' : 'CS2'}
+#     headers = {"Authorization": "KakaoAK " + api_key}
+#     places = requests.get(url, params=params, headers=headers).json()['documents']
     
-    for i in range(0, 3):
-        placeName.append(places[i]['place_name'])
-        placeAddr.append(places[i]['address_name'])
-        placeUrl.append(places[i]['place_url'])
+#     for i in range(0, 3):
+#         placeName.append(places[i]['place_name'])
+#         placeAddr.append(places[i]['address_name'])
+#         placeUrl.append(places[i]['place_url'])
 
-    total = list(zip(placeName, placeAddr, placeUrl))
+#     total = list(zip(placeName, placeAddr, placeUrl))
 
-    return total
+#     return total
 
 
 def musicSearch():
